180ff256137980cafc2f3d211345dff4638cab34
Loic Mohin <lmohin@z1r12p1.42lyon.fr>
2024-01-08 21:23:46 +0100

    forgot light.c

--- parsing/light.c
+++ parsing/light.c
@@ -6,39 +6,39 @@
 /*   By: TheTerror <jfaye@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 21:53:19 by TheTerror         #+#    #+#             */
-/*   Updated: 2024/01/06 11:23:55 by lmohin           ###   ########.fr       */
+/*   Updated: 2024/01/08 17:04:47 by lmohin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-t_bool	ft_set_pol(t_vars *v, char *infopol);
-t_bool	ft_set_lcolor(t_vars *v, char *infocolor);
+t_bool	ft_set_pol(t_vars *v, char *infopol, size_t line_index);
+t_bool	ft_set_lcolor(t_vars *v, char *infocolor, size_t line_index);
 
-t_bool ft_set_light(t_vars *v, char **infos)
+t_bool ft_set_light(t_vars *v, char **infos, size_t line_index)
 {
 	if (v->light)
-		return (ft_error("light: multiple definitions"));
+		return (scene_error("light: multiple definitions", line_index));
 	if (ft_2strlen(infos) < 2)
-		return (ft_error("light: missing information"));
+		return (scene_error("light: missing information", line_index));
 	if (ft_2strlen(infos) > 3)
-		return (ft_error("light: too much information"));
+		return (scene_error("light: too much information", line_index));
 	v->light = ft_calloc(1, sizeof(t_light));
 	if (!v->light)
 		return (ft_error("ft_set_light(): ft_calloc() failed"));
-	if (!ft_set_pol(v, infos[0]))
+	if (!ft_set_pol(v, infos[0], line_index))
 		return (__FALSE);
 	if (!ft_isnumber(infos[1]))
-		return (ft_error("light: expecting only decimal numbers"));
+		return (scene_error("light: expecting only decimal numbers", line_index));
 	v->light->ratio = ft_atod(infos[1]);
 	if (v->light->ratio < 0 || v->light->ratio > 1)
-		return (ft_error("light: invalid ratio"));
-	if (!ft_set_lcolor(v, infos[2]))
+		return (scene_error("light: invalid ratio", line_index));
+	if (!ft_set_lcolor(v, infos[2], line_index))
 		return (__FALSE);
 	return (__TRUE);
 }
 
-t_bool	ft_set_pol(t_vars *v, char *infopol)
+t_bool	ft_set_pol(t_vars *v, char *infopol, size_t line_index)
 {
 	char	**pol;
 	int		i;
@@ -49,12 +49,12 @@ t_bool	ft_set_pol(t_vars *v, char *infopol)
 		return (ft_error("ft_set_pol(): ft_splitwset() failed"));
 	if (!pol[0] || ft_2strlen(pol) != 3)
 		return (ft_free2str(&pol), \
-			ft_error("light: incorrect point of light format"));
+			scene_error("light: incorrect point of light format", line_index));
 	while (pol[i])
 	{
 		if (!ft_isnumber(pol[i]))
 			return (ft_free2str(&pol), \
-				ft_error("light: expecting only decimal numbers"));
+				scene_error("light: expecting only decimal numbers", line_index));
 		i++;
 	}
 	v->light->pol.x = ft_atod(pol[0]);
@@ -64,7 +64,7 @@ t_bool	ft_set_pol(t_vars *v, char *infopol)
 	return (__TRUE);
 }
 
-t_bool	ft_set_lcolor(t_vars *v, char *infocolor)
+t_bool	ft_set_lcolor(t_vars *v, char *infocolor, size_t line_index)
 {
 	char	**colors;
 	int		i;
@@ -75,12 +75,12 @@ t_bool	ft_set_lcolor(t_vars *v, char *infocolor)
 		return (ft_error("ft_set_lcolor(): ft_splitwset() failed"));
 	if (!colors[0] || ft_2strlen(colors) != 3)
 		return (ft_free2str(&colors), \
-			ft_error("light: incorrect color format"));
+			scene_error("light: incorrect color format", line_index));
 	while (colors[i])
 	{
 		if (!ft_isnumber(colors[i]))
 			return (ft_free2str(&colors), \
-				ft_error("light: expecting only decimal numbers"));
+				scene_error("light: expecting only decimal numbers", line_index));
 		i++;
 	}
 	v->light->rgb.r = ft_atoi(colors[0]);
@@ -89,6 +89,6 @@ t_bool	ft_set_lcolor(t_vars *v, char *infocolor)
 	ft_free2str(&colors);
 	if (v->light->rgb.r < 0 || v->light->rgb.r > 255 || v->light->rgb.g < 0 || v->light->rgb.g > 255 || \
 		v->light->rgb.b < 0 || v->light->rgb.b > 255)
-		return (ft_error("light: incorrect color format"));
+		return (scene_error("light: incorrect color format", line_index));
 	return (__TRUE);
 }
