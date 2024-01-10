2cb29ff7ef597141c1a17f5b702b8cfbedf692cf
Loic Mohin <lmohin@z4r2p2.42lyon.fr>
2024-01-10 17:23:22 +0100

    norming spheres parsing, some fixes

--- parsing/sphere.c
+++ parsing/sphere.c
@@ -6,14 +6,16 @@
 /*   By: TheTerror <jfaye@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 21:53:48 by TheTerror         #+#    #+#             */
-/*   Updated: 2024/01/10 16:45:25 by lmohin           ###   ########.fr       */
+/*   Updated: 2024/01/10 17:22:20 by lmohin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
 t_bool	set_sphere_center(t_sp *sp, char *infocenter, size_t line_index);
+t_bool	parse_sphere_center(char **center, size_t line_index);
 t_bool	set_sphere_color(t_sp *sp, char *infocolor, size_t line_index);
+t_bool	parse_sphere_color(char **colors, size_t line_index);
 
 t_bool	set_sphere(t_vars *v, char **infos, size_t line_index)
 {
@@ -27,11 +29,14 @@ t_bool	set_sphere(t_vars *v, char **infos, size_t line_index)
 		return (__FALSE);
 	v->sp[i] = ft_calloc(1, sizeof(t_sp));
 	if (!v->sp[i])
-		return (ft_error("ft_set_sphere(): ft_calloc() failed"));
+		return (ft_error("set_sphere(): ft_calloc() failed"));
 	if (!set_sphere_center(v->sp[i], infos[0], line_index))
 		return (__FALSE);
 	if (!ft_isnumber(infos[1]))
-		return (scene_error("sphere: expecting only decimal numbers", line_index));
+	{
+		return (scene_error("sphere: expecting only decimal numbers",
+				line_index));
+	}
 	v->sp[i]->d = ft_atod(infos[1]);
 	if (!set_sphere_color(v->sp[i], infos[2], line_index))
 		return (__FALSE);
@@ -41,21 +46,19 @@ t_bool	set_sphere(t_vars *v, char **infos, size_t line_index)
 t_bool	set_sphere_center(t_sp *sp, char *infocenter, size_t line_index)
 {
 	char	**center;
-	int		i;
 
-	i = 0;
-	center = ft_splitwset(infocenter, " ,\t\n");
-	if (!center)
-		return (ft_error("ft_set_center(): ft_splitwset() failed"));
-	if (!center[0] || ft_2strlen(center) != 3)
-		return (ft_free2str(&center), \
-			scene_error("sphere: incorrect center point format", line_index));
-	while (center[i])
+	if (ft_countchar(infocenter, ',') != 2)
 	{
-		if (!ft_isnumber(center[i]))
-			return (ft_free2str(&center), \
-				scene_error("sphere: expecting only decimal numbers", line_index));
-		i++;
+		return (scene_error("sphere: incorrect center point format",
+				line_index));
+	}
+	center = ft_split(infocenter, ',');
+	if (!center)
+		return (ft_error("set_center(): ft_split() failed"));
+	if (!parse_sphere_center(center, line_index))
+	{
+		ft_free2str(&center);
+		return (__FALSE);
 	}
 	sp->o.x = ft_atod(center[0]);
 	sp->o.y = ft_atod(center[1]);
@@ -64,31 +67,69 @@ t_bool	set_sphere_center(t_sp *sp, char *infocenter, size_t line_index)
 	return (__TRUE);
 }
 
+t_bool	parse_sphere_center(char **center, size_t line_index)
+{
+	int	i;
+
+	i = 0;
+	if (ft_2strlen(center) != 3)
+	{
+		return (scene_error("sphere: incorrect center point format",
+				line_index));
+	}
+	while (center[i])
+	{
+		if (!ft_isnumber(center[i]))
+		{
+			return (scene_error("sphere: incorrect center point format",
+					line_index));
+		}
+		i++;
+	}
+	return (__TRUE);
+}
+
 t_bool	set_sphere_color(t_sp *sp, char *infocolor, size_t line_index)
 {
 	char	**colors;
-	int		i;
 
-	i = 0;
 	colors = ft_splitwset(infocolor, " ,\t\n");
 	if (!colors)
 		return (ft_error("ft_set_spcolor(): ft_splitwset() failed"));
-	if (!colors[0] || ft_2strlen(colors) != 3)
-		return (ft_free2str(&colors), \
-			scene_error("sphere: incorrect color format", line_index));
-	while (colors[i])
+	if (!parse_sphere_color(colors, line_index))
 	{
-		if (!ft_isnumber(colors[i]))
-			return (ft_free2str(&colors), \
-				scene_error("sphere: expecting only decimal numbers", line_index));
-		i++;
+		ft_free2str(&colors);
+		return (__FALSE);
 	}
 	sp->rgb.r = ft_atoi(colors[0]);
 	sp->rgb.g = ft_atoi(colors[1]);
 	sp->rgb.b = ft_atoi(colors[2]);
 	ft_free2str(&colors);
-	if (sp->rgb.r < 0 || sp->rgb.r > 255 || sp->rgb.g < 0 || sp->rgb.g > 255 || \
-		sp->rgb.b < 0 || sp->rgb.b > 255)
+	if (sp->rgb.r < 0 || sp->rgb.r > 255 || sp->rgb.g < 0 || sp->rgb.g > 255
+		|| sp->rgb.b < 0 || sp->rgb.b > 255)
+	{
 		return (scene_error("sphere: incorrect color format", line_index));
+	}
+	return (__TRUE);
+}
+
+t_bool	parse_sphere_color(char **colors, size_t line_index)
+{
+	int	i;
+
+	i = 0;
+	if (ft_2strlen(colors) != 3)
+	{
+		return (scene_error("sphere: incorrect color format", line_index));
+	}
+	while (colors[i])
+	{
+		if (!ft_is_uint(colors[i]))
+		{
+			scene_error("sphere: incorrect color format", line_index);
+			return (__FALSE);
+		}
+		i++;
+	}
 	return (__TRUE);
 }
