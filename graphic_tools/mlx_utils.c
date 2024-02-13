3efd866ee06914227ed48d6a1b29167bb331c30a
Loic Mohin <lmohin@z3r6p5.42lyon.fr>
2024-02-13 14:40:57 +0100

    fix error messages mlx_utils

--- graphic_tools/mlx_utils.c
+++ graphic_tools/mlx_utils.c
@@ -6,25 +6,31 @@
 /*   By: TheTerror <jfaye@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 19:48:23 by TheTerror         #+#    #+#             */
-/*   Updated: 2024/02/12 17:26:35 by TheTerror        ###   ########lyon.fr   */
+/*   Updated: 2024/02/13 14:40:00 by lmohin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minirt.h"
 
+t_bool	mlx_error(char *error_msg)
+{
+	ft_putstr_fd(error_msg, 2);
+	return (__FALSE);
+}
+
 t_bool	set_mlx(t_vars *v)
 {
 	v->xptr = mlx_init();
 	if (!v->xptr)
-		return (__FALSE);
+		return (mlx_error("Error\nmlx_init() failed\n"));
 	v->win = mlx_new_window(v->xptr, _SIZE_X, _SIZE_Y, "miniRT");
 	if (!v->win)
-		return (__FALSE);
+		return (mlx_error("Error\nmlx_new_window failed\n"));
 	v->im->img = mlx_new_image(v->xptr, _WIDHT, _HEIGHT);
 	if (!v->im->img)
-		return (__FALSE);
-	v->im->addr = mlx_get_data_addr(v->im->img, &(v->im->bpp), \
-					&(v->im->size_line), &(v->im->endian));
+		return (mlx_error("Error\nmlx_new_image failed\n"));
+	v->im->addr = mlx_get_data_addr(v->im->img, &(v->im->bpp),
+			&(v->im->size_line), &(v->im->endian));
 	return (__TRUE);
 }
 
