c9c1887bfa9a379c5f3765a2a5a327a49c921bb1
Loic Mohin <lmohin@z3r3p1.42lyon.fr>
2024-02-13 03:12:24 +0100

    fix cylinder parsing

--- parsing/cylinder_utils.c
+++ parsing/cylinder_utils.c
@@ -6,7 +6,7 @@
 /*   By: TheTerror <jfaye@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 22:06:56 by TheTerror         #+#    #+#             */
-/*   Updated: 2024/02/11 13:17:47 by TheTerror        ###   ########lyon.fr   */
+/*   Updated: 2024/02/13 03:11:38 by lmohin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,17 @@ t_bool	set_cycenter(t_cy *cy, char *infocenter, size_t line_index)
 	int		i;
 
 	i = 0;
-	center = ft_splitwset(infocenter, " ,\t\n");
+	center = ft_split(infocenter, ',');
 	if (!center)
-		return (ft_error("ft_set_center(): ft_splitwset() failed"));
-	if (!center[0] || ft_2strlen(center) != 3)
+		return (ft_error("ft_set_center(): ft_split() failed"));
+	if (ft_2strlen(center) != 3)
 		return (ft_free2str(&center), \
-			scene_error("cylindre: incorrect center point format", line_index));
+			scene_error("cylinder: incorrect center point format", line_index));
 	while (center[i])
 	{
 		if (!isnumber(center[i]))
 			return (ft_free2str(&center), scene_error(\
-				"cylindre: expecting only decimal numbers", line_index));
+				"cylinder: expecting only decimal numbers", line_index));
 		i++;
 	}
 	cy->o.x = ft_atod(center[0]);
@@ -47,17 +47,17 @@ t_bool	set_cydir(t_cy *cy, char *infodir, size_t line_index)
 	int		i;
 
 	i = 0;
-	dir = ft_splitwset(infodir, " ,\t\n");
+	dir = ft_split(infodir, ',');
 	if (!dir)
-		return (ft_error("ft_set_dir(): ft_splitwset() failed"));
-	if (!dir[0] || ft_2strlen(dir) != 3)
+		return (ft_error("ft_set_dir(): ft_split() failed"));
+	if (ft_2strlen(dir) != 3)
 		return (ft_free2str(&dir), scene_error(\
-			"cylindre: incorrect orientation vector format", line_index));
+			"cylinder: incorrect orientation vector format", line_index));
 	while (dir[i])
 	{
 		if (!isnumber(dir[i]))
 			return (ft_free2str(&dir), scene_error(\
-				"cylindre: expecting only decimal numbers", line_index));
+				"cylinder: expecting only decimal numbers", line_index));
 		i++;
 	}
 	if (!set_cydir_op(cy, dir, line_index))
@@ -73,8 +73,11 @@ t_bool	set_cydir_op(t_cy *cy, char **dir, size_t line_index)
 	cy->axis.z = ft_atod(dir[2]);
 	if (cy->axis.x < -1 || cy->axis.x > 1 || cy->axis.y < -1 || \
 		cy->axis.y > 1 || cy->axis.z < -1 || cy->axis.z > 1)
-		return (scene_error(\
-			"cylindre: incorrect orientation vector format", line_index));
+		return (scene_error("cylinder: incorrect orientation vector format",
+				line_index));
+	if (__sq(cy->axis.x) + __sq(cy->axis.y) + __sq(cy->axis.z) != 1)
+		return (scene_error("cylinder: incorrect orientation vector format",
+				line_index));
 	return (__TRUE);
 }
 
@@ -84,17 +87,20 @@ t_bool	set_cycolor(t_cy *cy, char *infocolor, size_t line_index)
 	int		i;
 
 	i = 0;
-	colors = ft_splitwset(infocolor, " ,\t\n");
+	colors = ft_split(infocolor, ',');
 	if (!colors)
-		return (ft_error("set_cycolor(): ft_splitwset() failed"));
-	if (!colors[0] || ft_2strlen(colors) != 3)
-		return (ft_free2str(&colors), \
-			scene_error("cylindre: incorrect color format", line_index));
+		return (ft_error("set_cycolor(): ft_split() failed"));
+	if (ft_2strlen(colors) != 3)
+		return (ft_free2str(&colors),
+			scene_error("cylinder: incorrect color format", line_index));
 	while (colors[i])
 	{
-		if (!isnumber(colors[i]))
-			return (ft_free2str(&colors), scene_error(\
-				"cylindre: expecting only decimal numbers", line_index));
+		if (!is_uint(colors[i]))
+		{
+			ft_free2str(&colors);
+			return (scene_error("cylinder: expecting only decimal numbers",
+					line_index));
+		}
 		i++;
 	}
 	if (!set_cycolor_op(cy, colors, line_index))
@@ -110,6 +116,6 @@ t_bool	set_cycolor_op(t_cy *cy, char **colors, size_t line_index)
 	cy->rgb.b = ft_atoi(colors[2]);
 	if (cy->rgb.r < 0 || cy->rgb.r > 255 || cy->rgb.g < 0 || cy->rgb.g > 255 || \
 		cy->rgb.b < 0 || cy->rgb.b > 255)
-		return (scene_error("cylindre: incorrect color format", line_index));
+		return (scene_error("cylinder: incorrect color format", line_index));
 	return (__TRUE);
 }
